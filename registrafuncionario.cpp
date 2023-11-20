56c512fa4f7e71734ee92f72c01968e3f52860e4
MatheusSansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2023-11-20 15:25:43 -0300

    registrafuncionario

---
+++ registrafuncionario.cpp
@@ -0,0 +1,11 @@
+struct funcionario{
+    string Nome;
+    string contato;
+    string funcao;
+    /*bool pegouChave;*/
+}
+
+vector<Funcionario> listaFuncionarios;
+
+bool funcionariorRegistrado;
+
